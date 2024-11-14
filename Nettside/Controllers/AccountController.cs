6229f3c3eb6fd43d1cf49e64fdebbe442dd0fcf1
Tringak1 <tringakongjeli@hotmail.com>
2024-11-14 22:36:05 +0100

    Implemented authentication og authorization

--- Nettside/Controllers/AccountController.cs
+++ Nettside/Controllers/AccountController.cs
@@ -1,32 +1,41 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Kartverket16.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Nettside.Models;
 using UsersApp.ViewModels;
+
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace UsersApp.Controllers
+namespace Nettside.Controllers
 {
+
+    [Authorize] // requires authentication for all actions by default
     public class AccountController : Controller
     {
         private readonly SignInManager<Users> signInManager;
         private readonly UserManager<Users> userManager;
 
-        public AccountController(SignInManager<Users> signInManager, UserManager<Users> userManager)
+        public AccountController(SignInManager<Users> signInManager, UserManager<Users> userManager) // constructor to inject UserManager & SignInManager services.
         {
             this.signInManager = signInManager;
             this.userManager = userManager;
         }
 
+        [HttpGet]
+        [AllowAnonymous]  // GET method login page
         public IActionResult Login()
         {
             return View();
         }
 
+        [AllowAnonymous]
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel model)
         {
             if (ModelState.IsValid)
             {
+                // attempt to sign in the user with provided credentials
                 var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
 
                 if (result.Succeeded)
@@ -42,11 +51,45 @@ namespace UsersApp.Controllers
             return View(model);
         }
 
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> ProfilePage()
+        {
+            var currentUser = await userManager.GetUserAsync(User);
+
+            if (currentUser != null)
+            {
+                // creating a ViewModel with current user's details
+                var profilePageViewModel = new ProfilePageViewModel
+                {
+                    FirstName = currentUser.FirstName,
+                    LastName = currentUser.LastName,
+                    Email = currentUser.Email
+
+                };
+
+                return View(profilePageViewModel);
+
+            }
+            // show error notification
+            return RedirectToAction("Login");
+        }
+
+
+
+
+        [AllowAnonymous] // GET method for registration page
+        [HttpGet]
+
         public IActionResult Register()
         {
             return View();
         }
 
+        
+        [AllowAnonymous]  // allows anonymous users to attempt registration
         [HttpPost]
         public async Task<IActionResult> Register(RegisterViewModel model)
         {
@@ -57,7 +100,7 @@ namespace UsersApp.Controllers
                     FirstName = model.FirstName,
                     LastName = model.LastName,
                     Email = model.Email,
-                    UserName = model.Email,
+
                 };
 
                 var result = await userManager.CreateAsync(users, model.Password);
@@ -79,11 +122,13 @@ namespace UsersApp.Controllers
             return View(model);
         }
 
+
         public IActionResult VerifyEmail()
         {
             return View();
         }
 
+        [AllowAnonymous] // this allows anonymous users to submit the VerifyEmail form
         [HttpPost]
         public async Task<IActionResult> VerifyEmail(VerifyEmailViewModel model)
         {
@@ -140,17 +185,19 @@ namespace UsersApp.Controllers
                 }
                 else
                 {
-                    ModelState.AddModelError("", "Email not found!");
+                    ModelState.AddModelError("", "Email not found.");
                     return View(model);
                 }
             }
             else
             {
-                ModelState.AddModelError("", "Something went wrong. try again.");
+                ModelState.AddModelError("", "Something went wrong. Please try again.");
                 return View(model);
             }
         }
 
+
+        [HttpGet]
         public async Task<IActionResult> Logout()
         {
             await signInManager.SignOutAsync();
